dad4a862cb297905ef0400875d2be3d9c7791411
ayush <ayush.chaurasia68789@gmail.com>
2026-06-19 19:16:22 +0530

    Done

---
+++ import_export/app.js
@@ -0,0 +1,6 @@
+import a from './script.js' ; // default export
+import { st,age } from './script.js' ;   // named export
+// import { age } from './script.js' ;   // named export
+console.log(a) ; 
+console.log(st) ; 
+console.log(age) ; 
---
+++ import_export/script.js
@@ -0,0 +1,6 @@
+export var a = 20 ; 
+// agr bs ek xheez export krni h to use defaulkt export
+export default a ;
+
+export let st = "Ayush"  ; 
+export let age = 19  ; 
