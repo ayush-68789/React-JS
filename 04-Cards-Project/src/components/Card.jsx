a1b49a353e1ef480309874ecbbc5bc437975c51e
ayush <ayush.chaurasia68789@gmail.com>
2026-06-23 19:40:57 +0530

    Done Cards Project Using Props and Components

--- 04-Cards-Project/src/components/Card.jsx
+++ 04-Cards-Project/src/components/Card.jsx
@@ -1,17 +1,17 @@
-const Card = () => {
+const Card = (props) => {
     return (
         <div className="card">
             <div className="top">
-                <img src="https://www.vhv.rs/dpng/d/417-4179580_amazon-logo-png-free-download-searchpng-transparent-png.png" alt="" />
+                <img src={props.img} alt="" />
                 <button>Save</button>
             </div>
             <div className="center">
-                <span>Amazon</span>
-                <p>5 days ago</p>
-                <h2>Software Developer</h2>
+                <span>{props.company}</span>
+                <p>{props.posted}</p>
+                <h2>{props.role}</h2>
                 <div className="tags">
-                    <button>Part-Time</button>
-                    <button>Fresher</button>
+                    <button>{props.experience}</button>
+                    <button>{props.type}</button>
                 </div>
             </div>
             <div className="line">
@@ -19,8 +19,8 @@ const Card = () => {
             </div>
             <div className="bottom">
                 <div className="price">
-                    <span>$120/hr</span>
-                    <p>Mumbai, India</p>
+                    <span>{props.salary}</span>
+                    <p>{props.location}</p>
                 </div>
                 <button>Apply Now</button>
             </div>
